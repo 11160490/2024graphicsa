0872b8a99e0aa86f454e625c226fddc062a91c10
11160490 <114201440+11160490@users.noreply.github.com>
2024-03-14 16:01:29 +0800

    Update main.cpp

--- week_GLUT_10_lines/main.cpp
+++ week_GLUT_10_lines/main.cpp
@@ -1,14 +1,14 @@
-#include <GL/glut.h>///18 使用GLUT外掛
+#include <GL/glut.h>
 void display()
 {
     glutSolidTeapot(0.3);
     glutSwapBuffers();
 }
-int main(int argc,char *argv[])///138 main()函式
+int main(int argc,char *argv[])
 {
- glutInit(&argc, argv);///140開啟GLUT功能
- glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE | GLUT_DEPTH);///143設定顯示模式
- glutCreateWindow("GLUT Shapes");///145開啟GLUT視窗
- glutDisplayFunc(display);///148displat函式
- glutMainLoop();///174迴圈
+ glutInit(&argc, argv);
+ glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE | GLUT_DEPTH);
+ glutCreateWindow("GLUT Shapes");
+ glutDisplayFunc(display);
+ glutMainLoop();
 }
